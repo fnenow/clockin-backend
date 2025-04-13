ca911e23666a0113b74ef289d97e17c12c027c05
fnenow <fnenow@gmail.com>
2025-04-12 23:06:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { DateTime } = require('luxon');
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DB_URL,
   ssl: {
     rejectUnauthorized: false
   }
