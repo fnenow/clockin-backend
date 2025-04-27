7246b242d41d2f13a805c86dde6bd32e94e121ae
fnenow <fnenow@gmail.com>
2025-04-27 14:13:17 -0700

    Update timeRoutes.js

--- backend/routes/timeRoutes.js
+++ backend/routes/timeRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { addEntry, getEntries, updateEntry, deleteEntry } = require('../controllers/timeController');
+const { addEntry, getEntries, updateEntry, deleteEntry, parseWebhook } = require('../controllers/timeController');
 
 router.post('/', addEntry);
 router.get('/', getEntries);
 router.put('/:id', updateEntry);
 router.delete('/:id', deleteEntry);
+
+// New webhook route
+router.post('/webhook', parseWebhook);
 
 module.exports = router;
