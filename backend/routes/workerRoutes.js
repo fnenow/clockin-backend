75c5d8ab16473970c366eedfc42dadd16baf4a40
fnenow <fnenow@gmail.com>
2025-04-27 13:55:14 -0700

    Create workerRoutes.js

---
+++ backend/routes/workerRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { addWorker, getWorkers } = require('../controllers/workerController');
+
+router.post('/', addWorker);
+router.get('/', getWorkers);
+
+module.exports = router;
