612acc78d5f75b8a62a7e20d84709c8a2579b8a0
fnenow <fnenow@gmail.com>
2025-04-27 13:21:52 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; // <---- THIS IS VERY IMPORTANT
 
 const timeRoutes = require('./routes/timeRoutes');
 const workerRoutes = require('./routes/workerRoutes');
@@ -10,7 +10,7 @@ const payrollRoutes = require('./routes/payrollRoutes');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('frontend'));
+app.use(express.static('frontend')); // ✅
 
 app.use('/api/time', timeRoutes);
 app.use('/api/workers', workerRoutes);
