413bae2bb000bb50e56be6939c4aceddb5c5eb02
fnenow <fnenow@gmail.com>
2025-04-27 13:54:11 -0700

    Create timeRoutes.js

---
+++ backend/routes/timeRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const { addEntry, getEntries, updateEntry, deleteEntry } = require('../controllers/timeController');
+
+router.post('/', addEntry);
+router.get('/', getEntries);
+router.put('/:id', updateEntry);
+router.delete('/:id', deleteEntry);
+
+module.exports = router;
