0dc73826fbefe06a6a28aea88aa3537159e54ad1
fnenow <fnenow@gmail.com>
2025-04-27 13:42:44 -0700

    Delete backend directory

--- backend/routes/payrollRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getPayroll } = require('../controllers/payrollController');
-
-router.get('/', getPayroll);
-
-module.exports = router;
--- backend/routes/workerRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { addWorker, getWorkers } = require('../controllers/workerController');
-
-router.post('/', addWorker);
-router.get('/', getWorkers);
-
-module.exports = router;
