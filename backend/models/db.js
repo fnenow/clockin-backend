a2eba2520021e0e1655197ebcde31d79de098a10
fnenow <fnenow@gmail.com>
2025-04-28 11:19:06 -0700

    Update db.js

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.postgresql://postgres:ePvXuTjGStlgRlpHdqnKTGgNAQTdNHei@$,
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
